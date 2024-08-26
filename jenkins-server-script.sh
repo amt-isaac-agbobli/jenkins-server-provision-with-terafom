6eff2eab20950918fc5d5d748b86e4663524e738
Agbobli Isaac <agbobliisaackwadzo@gmail.com>
2024-08-26 13:39:04 +0000

    chore: Update Jenkins server script to install Jenkins, Git, Terraform, kubectl, and Docker

--- jenkins-server-script.sh
+++ jenkins-server-script.sh
@@ -27,3 +27,18 @@ sudo yum -y install terraform
 sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.23.6/bin/linux/amd64/kubectl
 sudo chmod +x ./kubectl
 sudo mkdir -p $HOME/bin && sudo cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
+
+# Remove any existing Docker packages
+for pkg in docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate 
+docker-logrotate docker-engine; do sudo yum remove -y $pkg; done
+sleep 5
+
+# Install Docker
+sudo amazon-linux-extras install -y docker
+sudo systemctl start docker
+sudo systemctl enable docker
+
+# Add the current user to the docker group
+sudo usermod -aG docker $USER
+newgrp docker
+sleep 5
